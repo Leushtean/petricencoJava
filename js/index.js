6f495ac347a90d13ad11a13e78ed8d0bf2988a44
Nicolae <tsepordei@gmail.com>
2023-04-02 22:54:01 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,91 @@
+"use strict";
+//
+// let a = 15;
+// console.log(a);
+//
+// let number = 5;
+// number = 10;
+// alert(number);
+// const leftBorderWidth = 1;
+// alert(leftBorderWidth)
+//
+// console.log(name);
+// var name = 'Nick';
+
+
+// {
+//     let result = 50;
+// }
+// console.log(result);
+//
+// alert(5);
+// [].push('a');
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+// console.log(`Width auto body: ${vehicleBodyWidth}mm, length: ${vehicleBodyLength}mm.`);
+
+// ['vruru', 'jkkj', 'iuhiuh'].map(a =>);
+
+// Request
+// data
+// response
+
+
+// camelCase
+// snake_case
+// UPPER_SNAKE_CASE
+// let COLOR_RED = '~F346782';
+// Kebab-case
+// PascalCase
+
+// const userName = "John";
+// let userNumber = 25;
+// userNumber = 24;
+// console.log(userName, userNumber);
+//
+// let number = 4.6;
+// console.log(4/0);
+//
+// const person = 'Alex';
+// console.log(person);
+//
+// const obj = {
+//     name: 'Nick',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj['name'], obj[`age`]);
+//
+// let arr = ['plumb.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=----------------------------=====================
+
+
+// const storeName = 'My awesome store';
+//
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alex', 'John', 'Sam'],
+//     products: {
+//         'soap': 20,
+//         'water': 5
+//     },
+//     open: true
+// }
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=----------------------------=====================
+
+const storeName = 'Super Shop';
+const storeDescription = {
+    buget: 10000,
+    employees: ['Alex', 'Anna', 'Nick'],
+    products: {
+        'milk': 5,
+        'bread': 2
+    },
+    open: true,
+};
---
+++ js/index2.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+/*
+
+const answer = prompt('Are you 18?', '18');
+console.log(answer);
+
+const answer = +prompt('Are you 18?', '18');
+console.log(answer + 10);
+
+
+
+const answers = [];
+
+answers[0] = prompt('your name?', '');
+answers[1] = prompt('your last name?', '');
+answers[2] = prompt('your age?', '');
+
+console.log(typeof(answers));
+
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`)
+
+const user = 'Nick';
+alert(`hello, ${user}`);
+console.log('arr' + '-object')
+
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+*/
